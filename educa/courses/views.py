c7560daa77f2057bbdc0398209ac4873bf83b555
abdeljalil97 <abdeljalilbensoudane97@gmail.com>
2021-09-13 13:46:16 +0100

    creating module and course table model

---
+++ educa/courses/views.py
@@ -0,0 +1,34 @@
+from django.views.generic.list import ListView
+from .models import Course
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, \
+DeleteView
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'courses/manage/course/form.html'
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
